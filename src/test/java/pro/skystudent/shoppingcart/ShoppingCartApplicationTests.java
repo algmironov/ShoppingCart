b0332f97bdd2fdf6aa9fbfecddc3934f00df5345
Alexey Mironov <al.g.mironov@yandex.ru>
2022-03-17 00:48:38 +0300

    Initial commit

---
+++ src/test/java/pro/skystudent/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skystudent.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
